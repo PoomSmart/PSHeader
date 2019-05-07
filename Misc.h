18cbf25b84367126dfcdeea1675d7e05aa28ca30
PoomSmart <thatchapon.unp@outlook.com>
2019-05-07 12:33:02 +0700

    Revert "Update Misc.h"
    This reverts commit 52650789a0ca10e3d3c037c9eb51b37fffae1a54.

--- Misc.h
+++ Misc.h
@@ -14,3 +14,6 @@
 #define fileExist(path) [[NSFileManager defaultManager] fileExistsAtPath:path]
 #define CFStringEqual(s1, s2) (CFStringCompare(s1, s2, kCFCompareCaseInsensitive) == kCFCompareEqualTo)
 #define stringEqual(str1, str2) ([str1 isEqualToString:str2])
+
+#define PSFindSymbolCallable(image, name) make_sym_callable(MSFindSymbol(image, name))
+#define PSFindSymbolReadable(image, name) make_sym_readable(MSFindSymbol(image, name))
