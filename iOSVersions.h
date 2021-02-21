31e432a04848eb7bcdaa74b9096b82bff23dec3b
PoomSmart <thatchapon.unp@outlook.com>
2021-02-21 13:39:16 +0700

    -

--- iOSVersions.h
+++ iOSVersions.h
@@ -4,6 +4,10 @@
 #import <CoreFoundation/CoreFoundation.h>
 #import <version.h>
 
+#ifndef kCFCoreFoundationVersionNumber_iOS_14_5
+#define kCFCoreFoundationVersionNumber_iOS_14_5 1775.113
+#endif
+
 #define IS_IOS_BETWEEN_EEX(start, end) (kCFCoreFoundationVersionNumber >= kCFCoreFoundationVersionNumber_ ## start && kCFCoreFoundationVersionNumber < kCFCoreFoundationVersionNumber_ ## end)
 
 #endif
