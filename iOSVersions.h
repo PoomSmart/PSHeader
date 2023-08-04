b836acf9c1f487879b74db63d077575e0bedc289
Thatchapon Unprasert <thatchapon.unp@outlook.com>
2023-08-04 09:30:26 +0700

    Update iOSVersions.h

--- iOSVersions.h
+++ iOSVersions.h
@@ -4,6 +4,10 @@
 #import <CoreFoundation/CoreFoundation.h>
 #import <version.h>
 
+#ifndef kCFCoreFoundationVersionNumber_iOS_17_0
+#define kCFCoreFoundationVersionNumber_iOS_17_0 2000
+#endif
+
 #define IS_IOS_BETWEEN_EEX(start, end) (kCFCoreFoundationVersionNumber >= kCFCoreFoundationVersionNumber_ ## start && kCFCoreFoundationVersionNumber < kCFCoreFoundationVersionNumber_ ## end)
 
 #endif
